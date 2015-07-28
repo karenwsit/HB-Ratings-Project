1755b76191f9f0557eac7b0499335a6f110aff34
Hackbright Student <info@hackbrightacademy.com>
2015-07-28 13:01:47 -0700

    Finished Step 1!

--- seed.py
+++ seed.py
@@ -2,25 +2,41 @@
 
 from model import User, Rating, Movie, connect_to_db, db
 from server import app
+from datetime import datetime
 
 
-# def load_users():
-#     """Load users from u.user into database."""
+def load_users():
+    """Load users from u.user into database."""
 
-#     user_data_file = open("seed_data/u.user")
-#     user_data = user_data_file.read().split("\n")
-#     for line in user_data:
-#         user_data_list = line.split("|")
-#         if user_data_list == ['']:
-#             continue
-#         user_id, age, zipcode = user_data_list[0], user_data_list[1], user_data_list[4]
-#         new_user = User(user_id=user_id, age=age, zipcode=zipcode)
-#         db.session.add(new_user)
-#         db.session.commit()
+    user_data_file = open("seed_data/u.user")
+    user_data = user_data_file.read().split("\n")
+    for line in user_data:
+        user_data_list = line.split("|")
+        if user_data_list == ['']:
+            continue
+        user_id, age, zipcode = user_data_list[0], user_data_list[1], user_data_list[4]
+        new_user = User(user_id=user_id, age=age, zipcode=zipcode)
+        db.session.add(new_user)
+    db.session.commit()
+    
 
 def load_movies():
     """Load movies from u.item into database."""
 
+    movie_data_file = open("seed_data/u.item")
+    movie_data = movie_data_file.read().split("\n")
+    for line in movie_data:
+        movie_data_list = line.split("|")
+        if movie_data_list == ['']:
+            continue
+        movie_id, title, released_at, imdb_url = int(movie_data_list[0]), movie_data_list[1], movie_data_list[2], movie_data_list[4]
+        formatted_movie_title = title[:-7]
+        if released_at == "":
+            continue
+        formatted_release_date = datetime.strptime(released_at, "%d-%b-%Y")
+        new_movie = Movie(movie_id=movie_id, title=formatted_movie_title, released_at=formatted_release_date, imdb_url=imdb_url)
+        db.session.add(new_movie)
+    db.session.commit()
 
 def load_ratings():
     """Load ratings from u.data into database."""
@@ -34,12 +50,13 @@ def load_ratings():
         movie_id, user_id, score = int(ratings_data_list[0]), int(ratings_data_list[1]), int(ratings_data_list[2])
         new_rating = Rating(movie_id=movie_id, user_id=user_id, score=score)
         db.session.add(new_rating)
-        db.session.commit()
+    db.session.commit()
 
 
 if __name__ == "__main__":
     connect_to_db(app)
+    db.create_all()
 
-    # load_users()
+    load_users()
     load_movies()
     load_ratings()
